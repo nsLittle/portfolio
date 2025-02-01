6c9a8782ddd0a94afffdd0f2ba233dd88a35cb3b
nsLittle <notsolittle88@gmail.com>
2025-02-01 13:45:13 -0500

    px => vw

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,8 +6,8 @@ const About = () => {
     const [activeIndex, setActiveIndex] = useState(0);
     const texts = [
         "I'm a software developer passionate about creating human-centered solutions by leveraging technology to make our lives simpler, more efficient, and more fulfilling.",
-        "Curiosity fuels the soul, and killed the cat.",
         "Master of priority juggling, nurturer of creativity, and finder of beauty in the mundane and chaotic—at life and at work.",
+        "Curiosity fuels the soul, and killed the cat.",
         "Cats have nine lives, and I have five cats.  The math is in my favor.",
         "Life’s greatest joys often come from small, thoughtful moments. Technology should be simple in utility and amplify one's joy.",
         "Whimsy? It’s a daily necessity. Period."
@@ -20,51 +20,41 @@ const About = () => {
         return () => clearInterval(interval);
     }, [texts.length]);
 
-    // const scrollRef = useRef(null);
-
-    // useEffect(() => {
-    //     const container = scrollRef.current;
-    //     const cloneIcons = () => {
-    //         if (container) {
-    //             const icons = container.children;
-    //             const clone = Array.from(icons).map((icon) => icon.cloneNode(true));
-    //             clone.forEach((node) => container.appendChild(node));
-    //         }
-    //     };
-
-    //     cloneIcons();
-    // }, []);
-
     return (
         <>
             <Header />
 
             <div className="container">
-                <div className="image-section">
-                    <img src="/images/selfie.png" alt="Selfie" />
-                </div>
-                <div className="box">
-                    <h2>About</h2>
-                    {texts.map((text, index) => (
-                        <p
-                            key={index}
-                            className={`dynamic-text ${index === activeIndex ? "visible" : "hidden"}`}
-                            style={{
-                                textShadow: "1px 1px 3px rgba(0, 0, 0, 0.1)",
-                                filter: "grayscale(100%)",
-                                color: "red",
-                                display: "flex",
-                                justifyContent: "center",
-                                alignItems: "center",
-                                textAlign: "center",
-                                marginTop: "85px",
-                                marginLeft: "35px",
-                            }}
-                        >
-                            {text}
-                        </p>
+                <div className="about-box">
+                    <div className="image-section">
+                        <img src="/images/selfie.png" alt="Selfie" />
+                    </div>
+                    <div className="box">
+                        <h2>About</h2>
+                        {texts.map((text, index) => (
+                            <p
+                                key={index}
+                                className={`dynamic-text ${index === activeIndex ? "visible" : "hidden"}`}
+                                style={{
+                                    textShadow: "1px 1px 3px rgba(0, 0, 0, 0.1)",
+                                    filter: "grayscale(100%)",
+                                    color: "red",
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    textAlign: "center",
+                                    marginTop: "15%",
+                                    maxWidth: "100%",
+                                    marginLeft: "auto",
+                                    marginRight: "20%",
+                                    
+                                }}
+                            >
+                                {text}
+                            </p>
 
-                    ))}
+                        ))}
+                    </div>
                 </div>
             </div>
 
