c5d282d82a1cea28379806cc4dce2ddeef468784
nsLittle <notsolittle88@gmail.com>
2025-05-04 06:47:20 -0400

    mobile resume bullets centered

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -28,7 +28,7 @@ export default function ResumePage() {
 
           <h5 className="text-3xl  pt-5">Fullstack Software Engineer</h5>
           <div className="job-title flex flex-row mt-4">
-            <p className="text-center sm:text-right ml-auto mr-0 pt-[5px] w-[95%] text-lg">
+            <p className="text-center sm:text-right ml-auto mr-0 pt-[5px] w-[90%] text-lg">
               Strategic administrator turned software engineer, I translate
               complexity into clarity, directing the ground operations of a $5M
               research lab, and designing-building applications by leveraging
@@ -57,7 +57,7 @@ export default function ResumePage() {
 
           <h5 className="text-3xl pt-10">Technical Skills</h5>
           <div className="flex justify-end mt-4">
-            <p className="text-center sm:text-right max-w-4xl text-lg">
+            <p className="text-center sm:text-right w-[90%] text-lg">
               JavaScript | React.js | Next.js | React Native | Node.js |
               Express.js | MongoDB | TypeScript | APIs | JWT Authentication |
               Swagger | Git | Postman | Mocha | Tailwind | CSS | Bootstrap |
@@ -67,20 +67,20 @@ export default function ResumePage() {
           <h5 className="text-3xl pt-10">Education</h5>
 
           <div className="job-title flex flex-col gap-x-2 mt-4">
-            <p className="font-bold text-base pt-[5px] text-lg">Parsity.io,</p>
-            <div className="text-base w-[90%] text-right ml-auto mr-0">
-              <ul className="space-y-2 text-lg">
+            <p className="font-bold text-base pt-[5px] text-xl">Parsity.io,</p>
+            <div className="text-base w-full text-center sm:text-right mx-auto ml-auto mr-0">
+              <ul className="text-lg">
                 <li>Software Engineering Program</li>
               </ul>
             </div>
           </div>
 
           <div className="job-title flex flex-col gap-x-2 mt-4">
-            <p className="font-bold text-base pt-[5px] text-lg">
+            <p className="font-bold text-base pt-[5px] text-xl">
               Smith College,{" "}
             </p>
-            <div className="text-base w-[90%] text-right ml-auto mr-0  text-lg">
-              <ul>
+            <div className="text-base w-full text-center sm:text-right ml-auto mr-0 mx-auto text-lg">
+              <ul className="text-lg">
                 <li>Major, anthropology</li>
                 <li>Minor, internationl relations</li>
               </ul>
@@ -88,13 +88,20 @@ export default function ResumePage() {
           </div>
 
           <h5 className="text-3xl pt-10">Project Experience</h5>
-          <div className="ob-title flex flex-row gap-x-2 mt-8">
-            <p className="text-base pt-[5px] text-center sm:text-right ml-auto mr-0 text-lg">
-              Community-Driven Habit Formation Platform |{" "}
-              <span className="font-bold text-xl">Habit App</span>
+
+          <div className="flex flex-col sm:hidden gap-y-1 mt-8 text-center">
+            <p className="font-bold text-xl">Habit App</p>
+            <p className="text-base text-lg">
+              Community-Driven Habit Formation Platform
             </p>
           </div>
-          <div className="text-base pt-5 w-full sm:w-[75%] text-center sm:text-right ml-auto mr-0">
+
+          <p className="hidden sm:block text-base pt-[5px] text-right ml-auto mr-0 text-lg mt-4">
+            Community-Driven Habit Formation Platform |{" "}
+            <span className="font-bold text-xl">Habit App</span>
+          </p>
+
+          <div className="text-base pt-5 w-full sm:w-[90%] text-center sm:text-right ml-auto mr-0">
             <ul className="text-center sm:text-right list-none space-y-2 text-lg">
               <li className="before:content-['•'] before:mr-4">
                 Delivered a full-stack mobile app that supports habit-building
@@ -108,13 +115,17 @@ export default function ResumePage() {
             </ul>
           </div>
 
-          <div className="job-title flex flex-row gap-x-2 mt-8">
-            <p className="text-base pt-[5px] text-center sm:text-right ml-auto mr-0">
-              Real-Time AI Reframing App |{" "}
-              <span className="font-bold text-xl">Reframer</span>
-            </p>
+          <div className="flex flex-col sm:hidden gap-y-1 mt-8 text-center">
+            <p className="font-bold text-xl">Reframer</p>
+            <p className="text-base text-lg">Real-Time AI Reframing App</p>
           </div>
-          <div className="text-base pt-5 w-full sm:w-[75%] text-center sm:text-right ml-auto mr-0">
+
+          <p className="hidden sm:block text-base pt-[5px] text-right ml-auto mr-0 text-lg mt-8">
+            Real-Time AI Reframing App |{" "}
+            <span className="font-bold text-xl">Reframer</span>
+          </p>
+
+          <div className="text-base pt-5 w-full sm:w-[90%] text-center sm:text-right ml-auto mr-0">
             <ul className="text-center sm:text-right list-none space-y-2 text-lg">
               <li className="before:content-['•'] before:mr-4">
                 Created a web app that transforms a user’s negative self-talk
@@ -128,13 +139,17 @@ export default function ResumePage() {
             </ul>
           </div>
 
-          <div className="job-title flex flex-row gap-x-2 mt-8">
-            <p className="text-base pt-[5px]  text-right ml-auto mr-0">
-              Contact Management App |{" "}
-              <span className="font-bold text-xl">Contact List</span>
-            </p>
+          <div className="flex flex-col sm:hidden gap-y-1 mt-8 text-center">
+            <p className="font-bold text-xl">Contact List</p>
+            <p className="text-base text-lg">Contact Management App</p>
           </div>
-          <div className="text-base pt-5 w-full sm:w-[75%] text-center sm:text-right ml-auto mr-0">
+
+          <p className="hidden sm:block text-base pt-[5px] text-right ml-auto mr-0 text-lg mt-8">
+            Contact Management App |{" "}
+            <span className="font-bold text-xl">Contact List</span>
+          </p>
+
+          <div className="text-base pt-5 w-full sm:w-[90%] text-center sm:text-right ml-auto mr-0">
             <ul className="text-center sm:text-right list-none space-y-2 text-lg">
               <li className="before:content-['•'] before:mr-4">
                 Expanded a starter Next.js project into a fully functional
@@ -147,13 +162,17 @@ export default function ResumePage() {
             </ul>
           </div>
 
-          <div className="job-title flex flex-row gap-x-2 mt-8">
-            <p className="text-base pt-[5px] ttext-center sm:text-right ml-auto mr-0">
-              Personal Developer Portfolio |{" "}
-              <span className="font-bold text-xl">Portfolio Site</span>
-            </p>
+          <div className="flex flex-col sm:hidden gap-y-1 mt-8 text-center">
+            <p className="font-bold text-xl">Portfolio Site</p>
+            <p className="text-base text-lg">Personal Developer Portfolio</p>
           </div>
-          <div className="text-base pt-5 w-full sm:w-[75%] text-center sm:text-right ml-auto mr-0">
+
+          <p className="hidden sm:block text-base pt-[5px] text-right ml-auto mr-0 text-lg mt-8">
+            Personal Developer Portfolio |{" "}
+            <span className="font-bold text-xl">Portfolio Site</span>
+          </p>
+
+          <div className="text-base pt-5 w-full sm:w-[90%] text-center sm:text-right ml-auto mr-0">
             <ul className="text-center sm:text-right list-none space-y-2 text-lg">
               <li className="before:content-['•'] before:mr-4">
                 Deployed a personal website to showcase projects, skills, and
@@ -166,14 +185,19 @@ export default function ResumePage() {
             </ul>
           </div>
 
-          <h5 className="text-3xl pt-10">Experience</h5>
-          <div className="job-title flex flex-row gap-x-2 mt-8">
-            <p className="text-base pt-[5px] text-center sm:text-right ml-auto mr-0">
-              2024 - present | Member |{" "}
-              <span className="font-bold text-xl">HopeMakers Collective</span>
-            </p>
+          <h5 className="text-3xl pt-10 mb-8">Experience</h5>
+          <div className="flex flex-col items-center sm:hidden text-center">
+            <p className="font-bold text-xl">HopeMakers Collective</p>
+            <p className="text-base text-lg">Member</p>
+            <p className="text-base text-lg">2024 – present</p>
           </div>
-          <div className="text-base pt-5 w-full sm:w-[75%] text-center sm:text-right ml-auto mr-0">
+
+          <p className="hidden sm:block text-base pt-[5px] text-right ml-auto mr-0 text-lg">
+            2024 – present | Member |{" "}
+            <span className="font-bold text-xl">HopeMakers Collective</span>
+          </p>
+
+          <div className="text-base pt-5 w-full sm:w-[90%] text-center sm:text-right ml-auto mr-0">
             <ul className="text-center sm:text-right list-none space-y-2 text-lg">
               <li className="before:content-['•'] before:mr-4">
                 Designed and built a mobile app enabling users to build
@@ -190,13 +214,18 @@ export default function ResumePage() {
               </li>
             </ul>
           </div>
-          <div className="job-title flex flex-row gap-x-2 mt-8">
-            <p className="text-base pt-[5px] text-center sm:text-right ml-auto mr-0">
-              2024 - present | Software Engineer Student |{" "}
-              <span className="font-bold text-xl">Parsity</span>
-            </p>
+
+          <div className="flex flex-col items-center sm:hidden mt-8 text-center">
+            <p className="font-bold text-xl">Parsity</p>
+            <p className="text-base text-lg">Software Engineer Student</p>
+            <p className="text-base text-lg">2024 – 2025</p>
           </div>
-          <div className="text-base pt-5 w-full sm:w-[75%] text-center sm:text-right ml-auto mr-0">
+          <p className="hidden sm:block text-base pt-[5px] text-right ml-auto mr-0 text-lg mt-8">
+            2024 – present | Software Engineer Student |{" "}
+            <span className="font-bold text-xl">Parsity</span>
+          </p>
+
+          <div className="text-base pt-5 w-full sm:w-[90%] text-center sm:text-right ml-auto mr-0">
             <ul className="text-center sm:text-right list-none space-y-2 text-lg">
               <li className="before:content-['•'] before:mr-4">
                 Built full-stack applications using the MERN stack with features
@@ -210,13 +239,18 @@ export default function ResumePage() {
               </li>
             </ul>
           </div>
-          <div className="job-title flex flex-row gap-x-2 mt-8">
-            <p className="text-base pt-[5px] text-center sm:text-right ml-auto mr-0">
-              2019 - 2013 | SuperHost |{" "}
-              <span className="font-bold text-xl">AirBnB</span>
-            </p>
+
+          <div className="flex flex-col items-center sm:hidden mt-8 text-center">
+            <p className="font-bold text-xl">AirBnB</p>
+            <p className="text-base text-lg">SuperHost</p>
+            <p className="text-base text-lg">2019 - 2023</p>
           </div>
-          <div className="text-base pt-5 w-full sm:w-[75%] text-center sm:text-right ml-auto mr-0">
+          <p className="hidden sm:block text-base pt-[5px] text-right ml-auto mr-0 text-lg mt-8">
+            2019 - 2023 | SuperHost |{" "}
+            <span className="font-bold text-xl">AirBnB</span>
+          </p>
+
+          <div className="text-base pt-5 w-full sm:w-[90%] text-center sm:text-right ml-auto mr-0">
             <ul className="text-center sm:text-right list-none space-y-2 text-lg">
               <li className="before:content-['•'] before:mr-4">
                 Transformed a $400K property into a $1M luxury rental by
@@ -230,15 +264,22 @@ export default function ResumePage() {
               </li>
             </ul>
           </div>
-          <div className="job-title flex flex-row gap-x-2 mt-8">
-            <p className="text-base pt-[5px] text-center sm:text-right ml-auto mr-0">
-              2010 - 2013 | Lab Administrator | Lieberman Lab |{" "}
-              <span className="font-bold text-xl">
-                Boston Children&apos;s Hospital
-              </span>
+
+          <div className="flex flex-col items-center sm:hidden mt-8 text-center">
+            <p className="font-bold text-xl">Boston Children&apos;s Hospital</p>
+            <p className="text-base text-lg">
+              Lab Administrator | Lieberman Lab
             </p>
+            <p className="text-base text-lg">2010 - 2013</p>
           </div>
-          <div className="text-base pt-5 w-full sm:w-[75%] text-center sm:text-right ml-auto mr-0">
+          <p className="hidden sm:block text-base pt-[5px] text-right ml-auto mr-0 text-lg mt-8">
+            2010 - 2013 | Lab Administrator | Lieberman Lab |{" "}
+            <span className="font-bold text-xl">
+              Boston Children&apos;s Hospital
+            </span>
+          </p>
+
+          <div className="text-base pt-5 w-full sm:w-[90%] text-center sm:text-right ml-auto mr-0">
             <ul className="text-center sm:text-right list-none space-y-2 text-lg">
               <li className="before:content-['•'] before:mr-4">
                 Managed $2.5M+ in annual biomedical research funding, ensuring
@@ -252,13 +293,20 @@ export default function ResumePage() {
               </li>
             </ul>
           </div>
-          <div className="job-title flex flex-row gap-x-2 mt-8">
-            <p className="text-base pt-[5px] text-center sm:text-right ml-auto mr-0">
-              2008 - 2010 | Program Manager | Center for Future Banking |{" "}
-              <span className="font-bold text-xl">MIT Media Lab</span>
+
+          <div className="flex flex-col items-center sm:hidden mt-8 text-center">
+            <p className="font-bold text-xl">MIT Media Lab</p>
+            <p className="text-base text-lg">
+              Program Manager | Center for Future Banking
             </p>
+            <p className="text-base text-lg">2008 - 2010</p>
           </div>
-          <div className="text-base pt-5 w-full sm:w-[75%] text-center sm:text-right ml-auto mr-0">
+          <p className="hidden sm:block text-base pt-[5px] text-right ml-auto mr-0 text-lg mt-8">
+            2008 - 2010 | Program Manager | Center for Future Banking |{" "}
+            <span className="font-bold text-xl">MIT Media Lab</span>
+          </p>
+
+          <div className="text-base pt-5 w-full sm:w-[90%] text-center sm:text-right ml-auto mr-0">
             <ul className="text-center sm:text-right list-none space-y-2 text-lg">
               <li className="before:content-['•'] before:mr-4">
                 Supprted the successful acquisition and management of $5M in
@@ -272,13 +320,20 @@ export default function ResumePage() {
               </li>
             </ul>
           </div>
-          <div className="job-title flex flex-row gap-x-2 mt-8">
-            <p className="text-base pt-[5px] text-center sm:text-rightml-auto mr-0">
-              Administrative 2007 - 2010 | Assistant | Cognitive Machines Group
-              | <span className="font-bold text-xl">MIT Media Lab</span>
+
+          <div className="flex flex-col items-center sm:hidden mt-8 text-center">
+            <p className="font-bold text-xl">MIT Media Lab</p>
+            <p className="text-base text-lg">
+              Lab Administrator | Cognitive Machines Group
             </p>
+            <p className="text-base text-lg">2007 - 2010</p>
           </div>
-          <div className="text-base pt-5 w-full sm:w-[75%] text-center sm:text-right ml-auto mr-0">
+          <p className="hidden sm:block text-base pt-[5px] text-right ml-auto mr-0 text-lg mt-8">
+            2007 - 2010 | Lab Administrator | Cognitive Machines Group |{" "}
+            <span className="font-bold text-xl">MIT Media Lab</span>
+          </p>
+
+          <div className="text-base pt-5 w-full sm:w-[90%] text-center sm:text-right ml-auto mr-0">
             <ul className="text-center sm:text-right list-none space-y-2 text-lg">
               <li className="before:content-['•'] before:mr-4">
                 Directed a $5M research portfolio, aligning over 300
